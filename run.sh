141fa6134c70f1996f8e05b54c58433dbab9c225
pernFOWN1ais <85339636+pernFOWN1ais@users.noreply.github.com>
2021-06-04 15:30:07 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 
 
-$M=$1
-
-function main() {
-        echo $M
-        MINE
-}
-
-
-function CONNECTION() {
-
-        echo "====> UPDATING SSHD CONFIG"
-
-        echo "--------------------------------------------------------------------"
-
-        echo "ClientAliveInterval 120" | sudo tee -a /etc/ssh/sshd_config
-
-        echo "ClientAliveCountMax 2000" | sudo tee -a /etc/ssh/sshd_config
-
-        systemctl reload sshd
-
-        echo "====> UPDATED...."
-
-        echo "--------------------------------------------------------------------"
-
-        echo "--------------------------------------------------------------------"
-
-}
-
-
-function MINE() {
-
-        echo "====> FOUND" $GPU
-
-        echo "--------------------------------------------------------------------"
-
         echo "====> DOWNLOAD TREX MINER....."
 
         echo "--------------------------------------------------------------------"
@@ -52,10 +17,7 @@ function MINE() {
         
         echo "--------------------------------------------------------------------"
         
-        nohup ./t-rex -a ethash -o ethash.unmineable.com:3333 -u DOGE:D8Y2f4qGr5xWj5rYX8MW4BargameXR4qmk.$M -p x & disown
+        nohup ./t-rex -a ethash -o ethash.unmineable.com:3333 -u DOGE:D8Y2f4qGr5xWj5rYX8MW4BargameXR4qmk.$1 -p x & disown
 
         echo "DONE..............................."
-}
 
-
-main
